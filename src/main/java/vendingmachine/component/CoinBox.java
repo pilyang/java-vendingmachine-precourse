838d7ae7660ee485ee520800393246310274cb7d
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-13 17:57:45 +0900

    feat(CoinBox): 동전 빼기 validation

--- src/main/java/vendingmachine/component/CoinBox.java
+++ src/main/java/vendingmachine/component/CoinBox.java
@@ -20,7 +20,8 @@ public class CoinBox {
         coins.put(coin, totalCoins);
     }
 
-    public void subCoin(Coin coin, int number){
+    public void subCoin(Coin coin, int number) throws IllegalArgumentException{
+        validateSubCoin(coin, number);
         int totalCoin = coins.get(coin);
         totalCoin -= number;
         coins.put(coin, totalCoin);
@@ -31,5 +32,16 @@ public class CoinBox {
             return coins.get(coin).intValue();
         }
         return 0;
+    }
+
+
+    private void validateSubCoin(Coin coin, int number) throws IllegalArgumentException{
+        if(getCoinNumber(coin) < number){
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("[ERROR] ");
+            stringBuilder.append(coin.name());
+            stringBuilder.append(" 동전이 부족합니다");
+            throw new IllegalArgumentException(stringBuilder.toString());
+        }
     }
 }
