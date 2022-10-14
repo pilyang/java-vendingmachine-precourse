72f82251b67a7926ce409765801f270b5261d0c9
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-14 21:10:40 +0900

    feat(ProductList): 상품리스트 생성

---
+++ src/main/java/vendingmachine/component/ProductList.java
@@ -0,0 +1,13 @@
+package vendingmachine.component;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ProductList {
+
+    private HashMap<Product, Integer> products;
+
+    public ProductList(){
+        products = new HashMap<>();
+    }
+}
