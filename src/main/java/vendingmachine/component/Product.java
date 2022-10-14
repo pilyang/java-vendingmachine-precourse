5feed559abb1b49a97ea0cd2eaee48873e4c6e6d
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-14 20:31:29 +0900

    feat(Product): product 생성

---
+++ src/main/java/vendingmachine/component/Product.java
@@ -0,0 +1,12 @@
+package vendingmachine.component;
+
+public class Product {
+
+    private final String name;
+    private int price;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+}
