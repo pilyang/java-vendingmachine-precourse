9bd4483a1759bf39ad8fac63bbeb1c0d2bbdd335
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-18 17:55:29 +0900

    feat(VendingMachine): 돈 투입

--- src/main/java/vendingmachine/component/VendingMachine.java
+++ src/main/java/vendingmachine/component/VendingMachine.java
@@ -4,10 +4,16 @@ public class VendingMachine {
 
     private CoinBox coinBox;
     private ProductList productList;
+    private int insertedMoney;
 
     public VendingMachine(){
+        insertedMoney = 0;
         coinBox = new CoinBox();
         productList = new ProductList();
+    }
+
+    public void insertMoney(int money){
+        this.insertedMoney += money;
     }
 
     public void addCoin(Coin coin, int number){
