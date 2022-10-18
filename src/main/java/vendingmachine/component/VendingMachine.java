01cf115047c6990d974c6077b2fb1e5a7adcfe64
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-18 17:45:51 +0900

    feat(VendingMachine): 동전 추가

--- src/main/java/vendingmachine/component/VendingMachine.java
+++ src/main/java/vendingmachine/component/VendingMachine.java
@@ -9,4 +9,8 @@ public class VendingMachine {
         coinBox = new CoinBox();
         productList = new ProductList();
     }
+
+    public void addCoin(Coin coin, int number){
+        coinBox.addCoin(coin, number);
+    }
 }
