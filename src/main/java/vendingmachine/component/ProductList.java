9ef73ac1781077fda5906d059995208120453a0b
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-18 17:05:14 +0900

    feat(ProductList): 이름으로 상품 검색, 이름 validation

--- src/main/java/vendingmachine/component/ProductList.java
+++ src/main/java/vendingmachine/component/ProductList.java
@@ -33,4 +33,11 @@ public class ProductList {
         int stock = products.get(product).intValue();
         products.put(product, stock-quantity);
     }
+
+    public Product findByName(String name) throws IllegalArgumentException{
+        return products.keySet().stream()
+                .filter(p -> p.getName().equals(name))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] " + name + "이라는 상품이 존재하지 않습니다."));
+    }
 }
