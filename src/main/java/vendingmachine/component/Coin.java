9e84c710bd685ed37c9c129ff7d0209ffa3aef50
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-20 15:31:29 +0900

    refactor(Coin): korean 표기 추가

--- src/main/java/vendingmachine/component/Coin.java
+++ src/main/java/vendingmachine/component/Coin.java
@@ -3,20 +3,27 @@ package vendingmachine.component;
 import java.util.Arrays;
 
 public enum Coin {
-    COIN_500(500),
-    COIN_100(100),
-    COIN_50(50),
-    COIN_10(10);
+    COIN_500(500, "500원"),
+    COIN_100(100, "100원"),
+    COIN_50(50, "50원"),
+    COIN_10(10, "10원");
 
     private final int amount;
+    private final String korean;
+
     public static final int COIN_TYPE_NUM = Coin.values().length;
 
-    Coin(final int amount) {
+    Coin(final int amount, final String korean) {
         this.amount = amount;
+        this.korean = korean;
     }
 
     public int amount(){
         return amount;
+    }
+
+    public String korean(){
+        return korean;
     }
 
     public int totalAmount(int number){
