5cf3939a61413018ca1d3a1b3a16a15dcff3d8a4
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-20 14:33:26 +0900

    feat(VendingMachineController): 자판기 생성

---
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -0,0 +1,12 @@
+package vendingmachine.controller;
+
+import vendingmachine.component.VendingMachine;
+
+public class VendingMachineController {
+
+    private VendingMachine vendingMachine;
+
+    public VendingMachineController(){
+        this.vendingMachine = new VendingMachine();
+    }
+}
